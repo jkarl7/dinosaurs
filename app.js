d24c957bee54ed7a6aeaa7a234e38a5b49afdaa2
Karl Jürgenson <karl5@windowslive.com>
2021-12-18 12:13:29 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,32 +1,80 @@
+(function () {
+    fetch("dino.json")
+        .then(response => response.json())
+        .then(json => console.log(json));
+})();
 
-    // Create Dino Constructor
-
-
-    // Create Dino Objects
-
-
-    // Create Human Object
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+function HumanData(name, feet, inches, weight, diet) {
+    this.name = name;
+    this.feet = feet;
+    this.inches = inches;
+    this.weight = weight;
+    this.diet = diet;
+}
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+function initHumanDataVariables(formInput) {
+    let name = "";
+    let feet = "";
+    let inches = "";
+    let weight = "";
+    let diet = "";
 
-    // Remove form from screen
+    for (const formVariable of formInput) {
+        if (formVariable.name == 'name') {
+            name = formVariable.value;
+        } else if (formVariable.name == 'feet') {
+            feet = formVariable.value;
+        } else if (formVariable.name == 'inches') {
+            inches = formVariable.value;
+        } else if (formVariable.name == 'weight') {
+            weight = formVariable.value;
+        } else if (formVariable.name == 'diet') {
+            diet = formVariable.value;
+        }
+    }
+    return new HumanData(name, feet, inches, weight, diet);
+}
+
+$("#btn").click(function (e) {
+    let formInput = $('#dino-compare').serializeArray();
+    console.log(formInput);
+    let human = initHumanDataVariables(formInput);
+    console.log("Name: " + human.name);
+    console.log("Feet: " + human.feet);
+    console.log("Inches: " + human.inches);
+    console.log("Weight: " + human.weight);
+    console.log("Diet: " + human.diet);
+});
+
+// Create Dino Constructor
+
+
+// Create Dino Objects
+
+
+// Create Human Object
+
+// Use IIFE to get human data from form
+
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
